3c3688133a02bd3f935fe7ad5c2f2b6f006b535b
KikoLabiano <flabiano@hotmail.com>
2019-01-23 21:51:41 +0100

    BBDD

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const sqlite3 = require('sqlite3').verbose();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -7,4 +8,10 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+  let db = new sqlite3.Database('./bbdd/MyCultureMusts.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+  });
 });
